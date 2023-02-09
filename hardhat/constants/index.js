f89814a4ce930539cea3f32656b465d62108512c
christianaojo <christianaojoofficial.com>
2023-02-09 13:44:22 +0100

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that was deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x279062F358A5aBe933d126ddC3CDe4CB97d63EDc";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
